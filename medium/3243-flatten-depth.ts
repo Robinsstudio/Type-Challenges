8777e40e21c4ac6df92f65b5d0bb170a3ba55950
Robinsstudio <robin.dosanjos@akur8.com>
2023-03-03 21:15:53 +0100

    3243 - FlattenDepth

---
+++ medium/3243-flatten-depth.ts
@@ -0,0 +1,52 @@
+/*
+  3243 - FlattenDepth
+  -------
+  by jiangshan (@jiangshanmeta) #medium #array
+
+  ### Question
+
+  Recursively flatten array up to depth times.
+
+  For example:
+
+  ```typescript
+  type a = FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2> // [1, 2, 3, 4, [5]]. flattern 2 times
+  type b = FlattenDepth<[1, 2, [3, 4], [[[5]]]]> // [1, 2, 3, 4, [[5]]]. Depth defaults to be 1
+  ```
+
+  If the depth is provided, it's guaranteed to be positive integer.
+
+  > View on GitHub: https://tsch.js.org/3243
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Extends<T, U> = T extends U ? true : false;
+
+type Flatten<T, D extends number, A extends unknown[]> = T extends [infer First, ...infer Rest]
+  ? [Extends<First, unknown[]>, Extends<A['length'], D>] extends [true, false]
+  ? [...Flatten<First, D, [...A, unknown]>, ...Flatten<Rest, D, A>]
+  : [First, ...Flatten<Rest, D, A>]
+  : [];
+
+type FlattenDepth<T, D extends number = 1> = Flatten<T, D, []>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '../utils'
+
+type cases = [
+  Expect<Equal<FlattenDepth<[]>, []>>,
+  Expect<Equal<FlattenDepth<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<FlattenDepth<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, [[5]]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 3>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 19260817>, [1, 2, 3, 4, 5]>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3243/answer
+  > View solutions: https://tsch.js.org/3243/solutions
+  > More Challenges: https://tsch.js.org
+*/
