78f1896190f2a6d170be1ebf119227ff960110e4
Robinsstudio <robin.dosanjos@akur8.com>
2023-06-06 18:52:51 +0200

    5360 - Unique

--- easy/898-includes.ts
+++ easy/898-includes.ts
@@ -18,7 +18,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Includes<T extends readonly unknown[], U> = T extends [infer First, ...infer Rest]
+export type Includes<T extends readonly unknown[], U> = T extends [infer First, ...infer Rest]
   ? Equal<First, U> extends true
   ? true
   : Includes<Rest, U>
