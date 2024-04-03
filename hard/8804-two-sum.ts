acc71603d29d6a05efa9c1bb39bf4a24169758d1
Robinsstudio <robin.dosanjos@akur8.com>
2024-04-03 19:54:39 +0200

    8804 - Two Sum

---
+++ hard/8804-two-sum.ts
@@ -0,0 +1,61 @@
+/*
+  8804 - Two Sum
+  -------
+  by PsiloLau (@Psilocine) #hard #array #math
+
+  ### Question
+
+  Given an array of integers `nums` and an integer `target`, return true if two numbers such that they add up to `target`.
+
+  For example
+
+  ```ts
+  type sum1 = TwoSum<[3, 2, 4], 6> // true
+  type sum2 = TwoSum<[2, 7, 11, 15], 15> // false
+  ```
+
+  > View on GitHub: https://tsch.js.org/8804
+*/
+
+/* _____________ Your Code Here _____________ */
+import { Includes } from "../easy/898-includes";
+import { ConstructTuple } from "../medium/7544-construct-tuple";
+
+type AllPairs<T> = T extends [infer First, ...infer Rest]
+  ? [...Pairs<[First, ...Rest]>, ...AllPairs<Rest>]
+  : [];
+
+type Pairs<T> = T extends [infer First, infer Second, ...infer Rest]
+  ? [[First, Second], ...Pairs<[First, ...Rest]>]
+  : [];
+
+type SumPairwise<T> = T extends [[infer First extends number, infer Second extends number], ...infer Rest]
+  ? [[...ConstructTuple<First>, ...ConstructTuple<Second>]['length'], ...SumPairwise<Rest>]
+  : [];
+
+type TwoSum<T extends number[], U extends number> = Includes<SumPairwise<AllPairs<T>>, U>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '../utils'
+
+type cases = [
+  Expect<Equal<TwoSum<[3, 3], 6>, true>>,
+  Expect<Equal<TwoSum<[3, 2, 4], 6>, true>>,
+  Expect<Equal<TwoSum<[2, 7, 11, 15], 15>, false>>,
+  Expect<Equal<TwoSum<[2, 7, 11, 15], 9>, true>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 0>, false>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 1>, false>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 2>, false>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 3>, true>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 4>, true>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 5>, true>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 6>, false>>,
+  Expect<Equal<TwoSum<[3, 2, 0], 2>, true>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/8804/answer
+  > View solutions: https://tsch.js.org/8804/solutions
+  > More Challenges: https://tsch.js.org
+*/
